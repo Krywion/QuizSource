27bb2ce9593bb73e2348f9e1d75ce73234a881d1
Krywion <krywionek.maks@gmail.com>
2024-07-22 12:40:59 +0200

    added hero section

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -4,7 +4,14 @@ import styles from "./navbar.module.css";
 export default function Navbar() {
   return (
         <div className={styles.container}>
-            Navbar
+            <div>
+                <a href="/">QuizSource</a>
+            </div>
+            <div>
+                <button className={styles.btn}>About</button>
+                <button className={styles.btn}>Sign in</button>
+
+            </div>
         </div>
   )
 }
